a82e723eb48c29b3cdab573d7e8e55f9affb213b
James Jeffrey <jamesljeffrey1995@gmail.com>
2020-06-02 23:09:25 +0100

    Rotation of sides are finished, clockwise on top and bottom, forward on left and right.

--- RubiksCube/rubicsCube.py
+++ RubiksCube/rubicsCube.py
@@ -1,6 +1,8 @@
 import random
 import pytest
 
+rotateLeftSide = [[5, 1, 4, 3], [1, 4, 3, 5]]
+
 
 class rubiksEnv:
     rubiksCube = {
@@ -78,6 +80,38 @@ class rubiksEnv:
                 # self.rubiksCube[4][i] = [tempRot[0][2-i], tempRot[1][2-i], tempRot[2][2-i]] This makes it go
                 # antiClockwise
                 self.rubiksCube[4][i] = [tempRot[2][i], tempRot[1][i], tempRot[0][i]]
+        elif move == "l":
+            tempLeft = []
+            tempRot = [self.rubiksCube[2][0],
+                       self.rubiksCube[2][1],
+                       self.rubiksCube[2][2]]
+            for i in range(4):
+                tempLeft.append([self.rubiksCube[int(rotateLeftSide[0][i])][0][0],
+                                 self.rubiksCube[int(rotateLeftSide[0][i])][1][0],
+                                 self.rubiksCube[int(rotateLeftSide[0][i])][2][0]])
+            print(tempLeft)
+            for i in range(4):
+                for e in range(3):
+                    self.rubiksCube[int(rotateLeftSide[1][i])][e][0] = tempLeft[i][e]
+            for i in range(len(tempRot)):
+                self.rubiksCube[2][i] = [tempRot[0][2-i], tempRot[1][2-i], tempRot[2][2-i]]
+                #self.rubiksCube[2][i] = [tempRot[2][i], tempRot[1][i], tempRot[0][i]]
+        elif move == "r":
+            tempLeft = []
+            tempRot = [self.rubiksCube[0][0],
+                       self.rubiksCube[0][1],
+                       self.rubiksCube[0][2]]
+            for i in range(4):
+                tempLeft.append([self.rubiksCube[int(rotateLeftSide[0][i])][0][2],
+                                 self.rubiksCube[int(rotateLeftSide[0][i])][1][2],
+                                 self.rubiksCube[int(rotateLeftSide[0][i])][2][2]])
+            print(tempLeft)
+            for i in range(4):
+                for e in range(3):
+                    self.rubiksCube[int(rotateLeftSide[1][i])][e][2] = tempLeft[i][e]
+            for i in range(len(tempRot)):
+                self.rubiksCube[0][i] = [tempRot[0][2-i], tempRot[1][2-i], tempRot[2][2-i]]
+                #self.rubiksCube[2][i] = [tempRot[2][i], tempRot[1][i], tempRot[0][i]]
 
     def main(self):
         self.displayCube()
