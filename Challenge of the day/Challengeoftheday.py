617feafef1252f77cdb724b7fedeb8559d1d6500
James Jeffrey <jamesljeffrey1995@gmail.com>
2020-06-02 09:01:42 +0100

    Fixed problem of the day

--- Challenge of the day/Challengeoftheday.py
+++ Challenge of the day/Challengeoftheday.py
@@ -1,7 +1,7 @@
 def main():
     listofnumbers = []
     for i in range(2000,3201):
-        if i % 7 == 0 and i % 5 == 0:
+        if i % 7 == 0 and i % 5 > 0:
             listofnumbers.append(i)
     print(listofnumbers)
 
