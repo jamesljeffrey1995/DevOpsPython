a90d207b9e760e858a815a465620e708819dc041
James Jeffrey <jamesljeffrey1995@gmail.com>
2020-06-02 09:00:24 +0100

    Change to challenge of the day

--- Challenge of the day/Challengeoftheday.py
+++ Challenge of the day/Challengeoftheday.py
@@ -1,6 +1,6 @@
 def main():
     listofnumbers = []
-    for i in range(2000,3200):
+    for i in range(2000,3201):
         if i % 7 == 0 and i % 5 == 0:
             listofnumbers.append(i)
     print(listofnumbers)
